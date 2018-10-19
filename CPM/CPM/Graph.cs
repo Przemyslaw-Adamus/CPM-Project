4303939846ae92a2829e91e7f73501bd6098e106
Przemyslaw-Adamus <32523362+Przemyslaw-Adamus@users.noreply.github.com>
2018-10-19 13:21:27 +0200

    Add class Graph

---
+++ CPM/CPM/Graph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPM
+{
+    class Graph
+    {
+        public IList<Activity> Activities { get; private set; }
+        public IList<Incident> Incidents { get; private set; }
+
+        Graph()
+        {
+            Incidents = new List<Incident>();
+            Activities = new List<Activity>();
+        }
+
+        public void LoadGaph()
+        {
+
+        }
+
+    }
+}
